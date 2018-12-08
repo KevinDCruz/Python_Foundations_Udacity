c2fba79d8c9c168c0567cf7b8b0d358eaaff8387
Kevin D'Cruz <dcruzkevin94@gmail.com>
2018-12-08 16:41:59 -0700

    draw_square()

--- Movie_reviews.py
+++ Movie_reviews.py
@@ -1,2 +1,17 @@
+import webbrowser
 class Movie():
-    
+    def __init__(self, movie_title, movie_storyline, poster_image, movie_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = movie_trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+class TV():
+    def __init__(self, series_title, series_description, series_poster, imdb_link):
+        self.title = series_title
+        self.description = series_description
+        self.poster_image = series_poster
+        self.imdb = imdb_link
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,35 @@
 import Movie_reviews
 
-toy_story = Movie_reviews.Movie()
+toy_story = Movie_reviews.Movie(
+    "Toy Story",
+    "Boys and his toys that come to life",
+    "https://images-na.ssl-images-amazon.com/images/I/51K8r9COEQL.jpg",
+    "https://www.youtube.com/watch?v=LDXYRzerjzU")
+
+print("Trailer link: "+ toy_story.trailer_youtube_url)
+
+avatar = Movie_reviews.Movie(
+    "Avatar",
+    "Marine on an alien planet",
+    "http://www.gstatic.com/tv/thumb/v22vodart/3542039/p3542039_v_v8_ac.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+print("Avatar Storyline: ", avatar.storyline)
+#avatar.show_trailer()
+
+queen = Movie_reviews.Movie(
+    "Bohemian Rhapsody",
+    "Story of Freddie Mercury",
+    "http://www.gstatic.com/tv/thumb/v22vodart/15444050/p15444050_v_v8_as.jpg",
+    "https://www.youtube.com/watch?v=mP0VHJYFOAU")
+
+print("Movie name: ",queen.title)
+queen.show_trailer()
+
+Supernatural = Movie_reviews.TV(
+    "Supernatural",
+    "Sam and Dean are brothers who hunt Monsters",
+    "https://images-na.ssl-images-amazon.com/images/I/51-vKSdEy%2BL.jpg",
+    "https://www.imdb.com/title/tt0460681/")
+
+print("IMDB Link: ", Supernatural.imdb)
