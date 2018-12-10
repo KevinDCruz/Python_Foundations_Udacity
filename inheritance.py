1095ada85bb688fd8e0475d2e30a6ce0f0529fa2
Kevin D'Cruz <dcruzkevin94@gmail.com>
2018-12-09 18:12:19 -0700

    Added files

--- inheritance.py
+++ inheritance.py
@@ -4,6 +4,10 @@ class Parent():
         self.lastname = last_name
         self.eyecolor = eye_color
 
+    def show_info(self):
+        print("Last Name:"+self.lastname)
+        print("Eye color:"+self.eyecolor)
+
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -11,9 +15,15 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.toys = number_of_toys
 
-#billy_cyrus = Parent("Cyrus","Green")
-#print(billy_cyrus.lastname)
+    def show_info(self):
+        print("Last Name:"+self.lastname)
+        print("Eye color:"+self.eyecolor)
+        print("Number of Toys:"+str(self.toys))
 
-miley_cyrus = Child("Cyrus", "Green", 2)
+billy_cyrus = Parent("Cyrus","Green")
+print(billy_cyrus.lastname)
+
+miley_cyrus = Child("Cyrus", "green", 2)
 print(miley_cyrus.lastname)
 print(miley_cyrus.toys)
+print(miley_cyrus.show_info())
